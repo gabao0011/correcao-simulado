8fa269dc70332532bc1484abba7ee9bda92ec777
gabao0011 <gabriel.dalponte05@gmail.com>
2026-04-23 13:05:02 -0300

    simulado

---
+++ app/Livewire/Produto/ProdutoEdit.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Livewire\Produto;
+
+use App\Models\Produto;
+use Livewire\Component;
+
+class ProdutoEdit extends Component
+{
+    public $nome;
+    public $valor;
+    public $qtd_estoque;
+    public $qtd_minima;
+    public $produtoId;
+
+    public function mount($id){
+        $produto = Produto::find($id);
+
+        if($produto == null){
+            session()->flash('error', 'Produto não encontrado');
+            return redirect()->route('produto.index');
+        }
+
+        $this->produtoId = $produto->produtoId;
+        $this->nome = $produto->nome;
+        $this->valor = $produto->valor;
+        $this->qtd_estoque = $produto->qtd_estoque;
+        $this->qtd_minima = $produto->qtd_minima;
+    }
+
+    public function update($id){
+
+    }
+
+    public function render()
+    {
+        return view('livewire.produto.produto-edit');
+    }
+}
